aff8aeed6262c375e1c2f436d4b797805628025b
llt3055 <llt30555@gmail.com>
2026-03-01 21:59:06 -0800

    create movie class and add private and public section

---
+++ lab15.cpp
@@ -0,0 +1,26 @@
+// COMSC-210 | Lab  15| Tianyi Cao
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+class Movie {
+
+private:
+//private data members
+    string title;
+    int yearReleased;
+    string screenWriter;
+
+public:
+   
+
+};
+
+int main() {
+    
+    return 0;
+}
